f2bf4853027b6d05f1b02f4503b9afcaef1d9e74
ianaack <ianaack@gmail.com>
2022-03-19 20:47:00 -0400

    Feature: Added ability to save tasks
    Saved task persist through page refresh due to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,17 +46,58 @@ function checkTime() {
 setInterval(checkTime(), 1000 * 60 * 5);
 
 function saveTasks() {
-  var text = document.querySelectorAll(".description");
-  var time = document.querySelectorAll(".hour");
-
-  var tasks = (text, time);
+  var inputTextarea9 = document.querySelector(".hour-9");
   $(".saveBtn").on("click", function () {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("hour-9", JSON.stringify(inputTextarea9.value));
   });
+  inputTextarea9.value = JSON.parse(localStorage.getItem("hour-9"));
+
+  var inputTextarea10 = document.querySelector(".hour-10");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-10", JSON.stringify(inputTextarea10.value));
+  });
+  inputTextarea10.value = JSON.parse(localStorage.getItem("hour-10"));
+
+  var inputTextarea11 = document.querySelector(".hour-11");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-11", JSON.stringify(inputTextarea11.value));
+  });
+  inputTextarea11.value = JSON.parse(localStorage.getItem("hour-11"));
+
+  var inputTextarea12 = document.querySelector(".hour-12");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-12", JSON.stringify(inputTextarea12.value));
+  });
+  inputTextarea12.value = JSON.parse(localStorage.getItem("hour-12"));
+
+  var inputTextarea13 = document.querySelector(".hour-13");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-13", JSON.stringify(inputTextarea13.value));
+  });
+  inputTextarea13.value = JSON.parse(localStorage.getItem("hour-13"));
+
+  var inputTextarea14 = document.querySelector(".hour-14");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-14", JSON.stringify(inputTextarea14.value));
+  });
+  inputTextarea14.value = JSON.parse(localStorage.getItem("hour-14"));
+
+  var inputTextarea15 = document.querySelector(".hour-15");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-15", JSON.stringify(inputTextarea15.value));
+  });
+  inputTextarea15.value = JSON.parse(localStorage.getItem("hour-15"));
+
+  var inputTextarea16 = document.querySelector(".hour-16");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-16", JSON.stringify(inputTextarea16.value));
+  });
+  inputTextarea16.value = JSON.parse(localStorage.getItem("hour-16"));
+
+  var inputTextarea17 = document.querySelector(".hour-17");
+  $(".saveBtn").on("click", function () {
+    localStorage.setItem("hour-17", JSON.stringify(inputTextarea17.value));
+  });
+  inputTextarea17.value = JSON.parse(localStorage.getItem("hour-17"));
 }
 saveTasks();
-
-function loadTasks() {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-}
-loadTasks();
